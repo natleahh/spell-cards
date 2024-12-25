0cb9a3abeb64fe1d8c2cde37ef78183db234d42b
natleahh <71596982+natleahh@users.noreply.github.com>
2024-12-25 13:38:31 +0000

    feat: add more sources and proficiency map to static

--- static.py
+++ static.py
@@ -1,2 +1,9 @@
-DATA_PATH = r"C:\Users\alten\Projects\dnd-randos\Pf2eTools\data"
-SOURCES = ["CRB"]
+DATA_PATH: str = r"C:\Users\alten\Projects\dnd-randos\Pf2eTools\data"
+SOURCES: list[str] = ["PC1", "CRB", "APG", "LOME"]
+BASE_PROFICIENCY_MAP: dict[int, str] = {
+    "untrained": 0,
+    "trained": 2,
+    "expert": 4,
+    "master": 6,
+    "legendary": 8
+}
