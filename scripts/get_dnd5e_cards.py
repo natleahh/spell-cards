fc411996860b112a6ef1c4a8d600cf2e02d623c5
natleahh <a.l.tengbe@gmail.com>
2025-01-02 15:21:06 +0000

    feat: add dnd5e cli

---
+++ scripts/get_dnd5e_cards.py
@@ -0,0 +1,56 @@
+
+import argparse
+import json
+from pathlib import Path
+import sys
+from typing import Optional
+
+from spellcard_dataclasses import custom, dndbeyond
+
+
+def parse_cli_args(argv: Optional[list[str]]):
+    parser = argparse.ArgumentParser()
+
+    data_input = parser.add_mutually_exclusive_group(required=True)
+
+    data_input.add_argument(
+        "--json_id",
+        type=int
+    )
+
+    data_input.add_argument(
+        "--json_path",
+        type=Path,
+    )
+    data_input.add_argument(
+        "--spell_names",
+        type=lambda s: s.replace("_", " ").title(),
+        nargs="+"
+    )
+    
+    parser.add_argument(
+        "--outpath", "-o",
+        type=str,
+    )
+    return parser.parse_args(argv)
+
+def main(argv: Optional[list[str]] = None):
+    args = parse_cli_args(argv)
+    if args.spell_names:
+        spells = custom.Dnd5eSpells.from_spell_names(args.spell_names)
+    else:
+        if args.json_id:
+            build = dndbeyond.Build.from_json_id(args.json_id)
+        elif args.json_path:
+            build = dndbeyond.Build.from_json_data(args.json_path.read_text())
+        spells = custom.Dnd5eSpells.from_dndbeyond_build(build)
+        
+
+    cards = spells.get_all_cards()
+
+    with (open(args.outpath, "w") if args.outpath is not None else sys.stdout) as output:
+        json.dump(cards, output, indent=4)
+            
+
+if __name__ == "__main__":
+    main()
