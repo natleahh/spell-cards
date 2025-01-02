e5f518c86074eccb89d4c830b72369d40371a516
natleahh <71596982+natleahh@users.noreply.github.com>
2025-01-02 18:44:15 +0000

    ref: changed json input names

--- scripts/get_dnd5e_cards.py
+++ scripts/get_dnd5e_cards.py
@@ -2,7 +2,6 @@
 import argparse
 import json
 from pathlib import Path
-import string
 import sys
 from typing import Optional
 
@@ -17,12 +16,12 @@ def parse_cli_args(argv: Optional[list[str]]):
     data_input = parser.add_mutually_exclusive_group(required=True)
 
     data_input.add_argument(
-        "--json_id",
+        "--character_json_id",
         type=int
     )
 
     data_input.add_argument(
-        "--json_path",
+        "--character_json_path",
         type=Path,
     )
     data_input.add_argument(
@@ -42,10 +41,10 @@ def main(argv: Optional[list[str]] = None):
     if args.spell_names:
         spells = custom.Dnd5eSpells.from_spell_names(args.spell_names)
     else:
-        if args.json_id:
-            build = dndbeyond.Build.from_json_id(args.json_id)
-        elif args.json_path:
-            build = dndbeyond.Build.from_json_data(args.json_path.read_text())
+        if args.character_json_id:
+            build = dndbeyond.Build.from_json_id(args.character_json_id)
+        elif args.character_json_path:
+            build = dndbeyond.Build.from_json_data(args.character_json_path.read_text())
         spells = custom.Dnd5eSpells.from_dndbeyond_build(build)
         
 
